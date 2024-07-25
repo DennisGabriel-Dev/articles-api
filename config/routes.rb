ddb055e342a3dc3fc9090952c89b75c43adeac61
DennisGabriel <dennisgabr745@gmail.com>
2024-07-25 00:57:43 -0300

    create routes for json formats

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :articles, constraints: ->(request) { request.format == :json }
   # Defines the root path route ("/")
   # root "articles#index"
 end
