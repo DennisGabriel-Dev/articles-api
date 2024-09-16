80df7ff2f5c1c006c579064f3ef61ce7b9dd96b6
DennisGabriel <dennisgabr745@gmail.com>
2024-09-16 00:26:14 -0300

    att tests for basic auth

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Articles' do
   before(:each) { Article.create(title: "art 2", description: "Content article 2") }
+  let(:header) { http_basic_auth_header }
   context 'GET' do
     it 'is response ok?' do
-      get '/api/v1/articles.json'
+      get '/api/v1/articles.json', headers: header
 
       expect(response).to have_http_status(:ok)
     end
 
     it 'content was included on response' do
-      get '/api/v1/articles.json' do
+      get '/api/v1/articles.json', headers: header do
         resposta = json_to_hash(response.body)
 
         expect(resposta[:data].first).not_to have_key(:created_at)
---
+++ spec/support/authentication.rb
@@ -0,0 +1,12 @@
+module Authentication
+  include ActionController
+  def http_basic_auth_header
+    @user = User.first_or_create(email: "x@x.com", password: "123456")
+    {
+      'HTTP_AUTHORIZATION':
+      ActionController::HttpAuthentication::Basic.encode_credentials(
+        @user.email, @user.password
+      )
+    }
+  end
+end
